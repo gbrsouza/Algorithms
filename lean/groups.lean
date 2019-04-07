91e7ba13712049d883de50808519592da3633c33
gabrielArSouza <gabriel_feg@hotmail.com>
2019-04-07 18:13:54 -0300

    complete exercices 01

--- lean/groups.lean
+++ lean/groups.lean
@@ -18,7 +18,7 @@ axiom ass : ∀ a b c : α, dot (dot a b ) c = dot a (dot b c)
 -- identity
 axiom iden : ∃e : α, ∀a : α, (dot e a = a ∧ dot a e = a)
 
---inverse
+-- inverse
 axiom inv : ∀a : α, ∃b : α, ( dot a b = e ∧ dot b a = e)
 
 end groups
