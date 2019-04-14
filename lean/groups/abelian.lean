1399a8f9ba84d9dc7f3006eb6a103b27d6a646f5
gabrielArSouza <gabriel_feg@hotmail.com>
2019-04-14 15:53:14 -0300

    correct groups

--- lean/groups/abelian.lean
+++ lean/groups/abelian.lean
@@ -1,12 +1,11 @@
 namespace abelian 
 
 variable U : Type
-variables e c : U 
+variables e : U 
 variable dot : U → U → U 
 
 local infix `⬝` := dot 
 
-axiom cls : ∀ a b, (a ⬝ b) = c -- closure
 axiom ass : ∀ a b c : U, (a ⬝ b) ⬝ c = a ⬝ (b ⬝ c) -- associativity  
 axiom iden : ∃e : U, ∀a : U, ((e ⬝ a) = a) ∧ ((a ⬝ e) = a) -- identity
 axiom inv : ∀a : U, ∃b : U, ((a ⬝ b) = e) ∧ ((b ⬝ a) = e)  -- inverse
--- lean/groups/groups.lean
+++ lean/groups/groups.lean
@@ -2,16 +2,13 @@ namespace groups
 
 variable α : Type
 
--- define the neutral and generic elements
-variables e c : α 
+-- define the neutral  elements
+variables e  : α 
 
 -- define the relation
 variable dot : α → α → α 
 
 -- axioms
--- closure
-axiom cls : ∀ a b, dot a b = c
-
 -- associativity
 axiom ass : ∀ a b c : α, dot (dot a b ) c = dot a (dot b c)  
 
