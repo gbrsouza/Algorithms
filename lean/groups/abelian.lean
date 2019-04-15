da9aa134c3ae344380c55d7ad92145dd44c8f5ed
gabrielArSouza <gabriel_feg@hotmail.com>
2019-04-14 21:07:30 -0300

    abelian's groups

--- lean/groups/abelian.lean
+++ lean/groups/abelian.lean
@@ -1,14 +1,15 @@
-namespace abelian 
 
 variable U : Type
 variables e : U 
 variable dot : U → U → U 
 
-local infix `⬝` := dot 
+local infix `+` := dot 
 
-axiom ass : ∀ a b c : U, (a ⬝ b) ⬝ c = a ⬝ (b ⬝ c) -- associativity  
-axiom iden : ∃e : U, ∀a : U, ((e ⬝ a) = a) ∧ ((a ⬝ e) = a) -- identity
-axiom inv : ∀a : U, ∃b : U, ((a ⬝ b) = e) ∧ ((b ⬝ a) = e)  -- inverse
-axiom com : ∀ a b : U, (a ⬝ b) = (b ⬝ a)  -- commutativity
+axiom ass : ∀ a b c : U, (a + b) + c = a + (b + c) -- associativity  
+axiom iden : ∃e : U, ∀a : U, ((e + a) = a) ∧ ((a + e) = a) -- identity
+axiom inv : ∀a : U, ∃b : U, ((a + b) = e) ∧ ((b + a) = e)  -- inverse
+axiom com : ∀ a b : U, (a + b) = (b + a)  -- commutativity
 
-end abelian
+variables x y z : U
+
+example : x + (y + z) = x + (z + y) := sorry
